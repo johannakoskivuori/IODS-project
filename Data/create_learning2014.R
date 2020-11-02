64949830d8ae1c176dffe68dbf6307d1e670ba19
Johanna Koskivuori <johanna.koskivuori@uef.fi>
2020-11-02 16:39:24 +0200

    Data creating started

---
+++ Data/create_learning2014.R
@@ -0,0 +1,13 @@
+# Johanna Koskivuori, 02112020, R script for data wrangling in RStudio excercise 2.
+# More information about the data you can find : https://www.mv.helsinki.fi/home/kvehkala/JYTmooc/JYTOPKYS3-meta.txt 
+
+learning2014 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
+str(learning2014) #183 obs. of  60 variables
+dim(learning2014) # rows 183, columns 60
+
+install.packages("dplyr")
+library(dplyr) # useful library for data wrangling
+
+keep_columns <- c("gender","Age","Attitude", "deep", "Stra", "Surf", "Points") #variables gender, age, attitude, deep, stra, surf and points
+analysis_dataset <- select(learning2014, one_of(keep_columns))
+
