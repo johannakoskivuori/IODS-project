25336c0564d836f5356151040640b08c57e34a6f
Johanna Koskivuori <johanna.koskivuori@uef.fi>
2020-11-18 11:48:54 +0200

    Exercise 4 and human data

--- Data/create_human.R
+++ Data/create_human.R
@@ -79,3 +79,102 @@ write.table(human, file = "human.txt", sep = ",", col.names = TRUE, row.names = 
 human_created <- read.table("human.txt", sep = ",", header = TRUE) 
 str(human_created) # 195 obs. of  17 variables, works fine
  
+
+
+
+
+
+# Johanna Koskivuori, 17112020, Exercise 5 data wrangling continues
+
+# More information about the data is in: http://hdr.undp.org/en/content/human-development-index-hdi
+
+human1 <- read.table("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human1.txt", sep=",", header=TRUE)
+
+str(human1) #195 obs. of  19 variables
+dim(human1)#195 rows and 19 columns
+colnames(human1)
+#[1] "HDI.Rank"       "Country"        "HDI"            "Life.Exp"       "Edu.Exp"        "Edu.Mean"      
+#[7] "GNI"            "GNI.Minus.Rank" "GII.Rank"       "GII"            "Mat.Mor"        "Ado.Birth"     
+#[13] "Parli.F"        "Edu2.F"         "Edu2.M"         "Labo.F"         "Labo.M"         "Edu2.FM"       
+#[19] "Labo.FM"
+
+#Here are the explanations for variable names which have been shortened:
+
+#"Country" = Country name
+
+# Health and knowledge
+
+#"GNI" = Gross National Income per capita
+#"Life.Exp" = Life expectancy at birth
+#"Edu.Exp" = Expected years of schooling 
+#"Mat.Mor" = Maternal mortality ratio
+#"Ado.Birth" = Adolescent birth rate
+
+# Empowerment
+
+#"Parli.F" = Percetange of female representatives in parliament
+#"Edu2.F" = Proportion of females with at least secondary education
+#"Edu2.M" = Proportion of males with at least secondary education
+#"Labo.F" = Proportion of females in the labour force
+#"Labo.M" " Proportion of males in the labour force
+
+#"Edu2.FM" = Edu2.F / Edu2.M
+#"Labo.FM" = Labo2.F / Labo2.M
+
+
+library(stringr)
+
+# remove the commas from GNI and create a numeric version of it
+human1 <- mutate(human1, GNI=str_replace(GNI, pattern=",", replace =""))
+str(human1)
+human1$GNI <- as.numeric(human1$GNI)
+
+# Excluding unneeded variables, columns to keep are:
+keep <- c("Country", "Edu2.FM", "Labo.FM",  "Edu.Exp", "Life.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+
+# selecting the 'keep' columns
+human1 <- select(human1, one_of(keep))
+
+# printing out a completeness indicator of the 'human1' data
+complete.cases(human1)
+
+# printing out the data along with a completeness indicator as the last column
+data.frame(human1[-1], comp = complete.cases(human1))
+
+# filtering out all rows with missing (NA) values
+human2 <- filter(human1, complete.cases(human1) == TRUE)
+str(human2)#162 obs. of  9 variables
+
+#Removing the observations which relate to regions instead of countries
+
+# looking at the last 10 observations of human2
+tail(human2, 10) #7 regionsin the end
+
+# defining the last indice we want to keep
+last <- nrow(human2) - 7
+
+# choosing everything until the last 7 observations
+human3 <- human2[1:155, ]
+
+# adding countries as rownames
+rownames(human3) <- human3$Country
+tail(human3)
+
+# removing the Country variable from the data
+human3 <- select(human3, -Country)
+colnames(human3)
+str(human3)#155 obs. of  8 variables
+
+setwd("~/R/IODS-course/IODS-project/Data")
+
+# Saving the analysis dataset with rownames
+
+write.table(human3, file = "human.txt", sep = ",", col.names = TRUE, row.names = TRUE)
+human_created2 <- read.table("human.txt", sep = ",", header = TRUE) 
+str(human_created2) # 155 obs. of  8 variables, works fine
+
+
+
+
+
+
